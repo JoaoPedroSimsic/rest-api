d301dcc0d8ae7bc7ce602abf2958a249ddf24cdc
Joao <jpsimsic@hotmail.com>
2025-01-01 23:29:13 -0300

    user controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,13 +3,12 @@ import User from '../models/user';
 class UserController {
   async store(req, res) {
     try {
+      console.log(req.body);
       const newUser = await User.create(req.body);
       const { id, name, email } = newUser;
       return res.json({ id, name, email });
     } catch (e) {
-      return res.status(400).json({
-        errors: e.errors.map((err) => err.message),
-      });
+      return res.status(400);
     }
   }
 
--- src/models/User.js
+++ src/models/User.js
@@ -1,68 +1,54 @@
-import Sequelize, { Model } from "sequelize";
-import bcryptjs from "bcryptjs";
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../../config/database';
+import bcryptjs from 'bcryptjs';
 
-export default class User extends Model {
-  static init(sequelize) {
-    super.init(
-      {
-        id: {
-          type: Sequelize.INTEGER,
-          defaultValue: "",
-          autoIncrement: true,
-          primaryKey: true,
-        },
-        name: {
-          type: Sequelize.STRING,
-          defaultValue: "",
-          validate: {
-            len: {
-              args: [10, 50],
-              msg: "Your name should be from 10 to 50 characters long",
-            },
-          },
-        },
-        email: {
-          type: Sequelize.STRING,
-          defaultValue: "",
-          unique: {
-            msg: "An account with this email already exists",
-          },
-          validate: {
-            isEmail: {
-              msg: "Please, enter a valid email",
-            },
-          },
-        },
-        password: {
-          type: Sequelize.VIRTUAL,
-          defaultValue: "",
-          validate: {
-            len: {
-              args: [6, 50],
-              msg: "The password must have 6 to 50 characters",
-            },
-          },
-        },
-        password_hash: {
-          type: Sequelize.STRING,
-          defaultValue: "",
+class User extends Model {}
+User.init(
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [10, 50],
+          msg: 'Your name should be from 10 to 50 characters long',
         },
       },
-      {
-        sequelize,
-      }
-    );
-
-    this.addHook("beforeSave", async (usr) => {
-      if (usr.password) {
-        usr.password_hash = await bcryptjs.hash(usr.password, 8);
-      }
-    });
-
-    return this;
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg: 'An account with this email already exists',
+      },
+      validate: {
+        isEmail: {
+          msg: 'Please, enter a valid email',
+        },
+      },
+    },
+    password: {
+      type: DataTypes.VIRTUAL,
+      validate: {
+        len: {
+          args: [6, 50],
+          msg: 'The password must have 6 to 50 characters',
+        },
+      },
+      set(value) {
+        this.setDataValue('password', value);
+        this.setDataValue('password_hash', bcryptjs.hashSync(value, 8));
+      },
+    },
+    password_hash: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'User',
   }
+);
 
-  passwordCheck(password) {
-    return bcryptjs.compare(password, this.password_hash);
-  }
-}
+export default User;
